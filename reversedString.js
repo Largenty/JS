e8e02665fd89da15f46dca51bbea421c5facee5e
Largenty <largenty@gmail.com>
2021-09-20 23:18:25 +0200

    reversedstring

---
+++ reversedString.js
@@ -0,0 +1,15 @@
+// Reverse a word
+// ex: word -> drow
+
+function reversedWord(str) {
+    const splitString = str.split('')
+    const reverseArray = splitString.reverse()
+    const joinArray = reverseArray.join('')
+    return joinArray
+}
+
+// or
+
+function solution(str){
+    return str.split('').reverse().join('')
+}
